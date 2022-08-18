1e4398c40613bdd9d217c485e356d17608fc8888
Michal <michalbialacki@gmail.com>
2022-08-18 23:35:19 +0200

    5th Update - Added: Spinning card animation & berry trivia

--- app/src/main/java/com/kkp/berrydex/berrydetail/BerryDetailScreen.kt
+++ app/src/main/java/com/kkp/berrydex/berrydetail/BerryDetailScreen.kt
@@ -5,6 +5,7 @@ import android.widget.Space
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -19,6 +20,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -103,6 +105,7 @@ fun BerryCard(
     berryInfo: Berry,
     modifier: Modifier = Modifier
 ) {
+
     Box(modifier = Modifier
         .padding(
             top = 60.dp,
@@ -170,27 +173,83 @@ fun BerrySpec(
 fun BerryNatGift(
     berryInfo: Berry
 ) {
+    var rotated by remember {
+        mutableStateOf(false)
+    }
+    val rotation by animateFloatAsState(
+        targetValue = if (rotated) 180f else 0f,
+        animationSpec = tween(500))
+    val frontAnimation by animateFloatAsState(
+        targetValue = if (!rotated) 1f else 0f,
+        animationSpec = tween(500)
+    )
+    val backAnimation by animateFloatAsState(
+        targetValue = if (rotated) 1f else 0f,
+        animationSpec = tween(500)
+    )
+
     Box(modifier = Modifier
         .width(300.dp)
         .height(120.dp)
         .padding(8.dp)
         .clip(RoundedCornerShape(10.dp))
-        .background(Color.LightGray)) {
+        .background(Color.LightGray)
+        .graphicsLayer {
+            rotationY = rotation
+            cameraDistance = 8 * density
+        }
+        .clickable {
+            rotated = !rotated
+        }){
         Column(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier.fillMaxSize()
+                .graphicsLayer {
+                    alpha = if (rotated) backAnimation else frontAnimation
+                    rotationY = rotation
+                },
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(
-                text = "Natural Gift",
-                fontWeight = FontWeight.SemiBold,
-                fontSize = 20.sp,
-                color = MaterialTheme.colors.onSurface)
-            NatGiftStats(
-                statName = "Type",
-                statValue = berryInfo.natural_gift_type.name.replaceFirstChar { it.uppercase() }
-            )
-            NatGiftStats(statName = "Power", statValue = berryInfo.natural_gift_power.toString())
+            if (!rotated){
+                Text(
+                    text = "Natural Gift",
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 20.sp,
+                    color = MaterialTheme.colors.onSurface)
+                NatGiftStats(
+                    statName = "Type",
+                    statValue = berryInfo.natural_gift_type.name.replaceFirstChar { it.uppercase() }
+                )
+                NatGiftStats(
+                    statName = "Power",
+                    statValue = berryInfo.natural_gift_power
+                        .toString()
+                )
+            }
+            else{
+                val berryFirmness = berryInfo.firmness.name.replace('-',' ')
 
+                val berryTrivia = "This berry feels ${berryFirmness} in your hand. " +
+                        "It measures about ${berryInfo.size / 10} centimeters. " +
+                        "You may gather them every ${4 * berryInfo.growth_time} hours " +
+                        "and - if cared properly - it can grow up to ${berryInfo.max_harvest}" +
+                        " berries per tree."
+                Text(
+                    text = "Usefull Info",
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 18.sp,
+                    color = MaterialTheme.colors.onSurface)
+                Text(
+                    text = "${berryTrivia} ",
+                    fontWeight = FontWeight.Normal,
+                    fontSize = 14.sp,
+                    textAlign = TextAlign.Justify,
+                    color = MaterialTheme.colors.onSurface,
+                    modifier = Modifier.padding(
+                        start = 4.dp,
+                        end = 4.dp
+                    )
+                )
+            }
         }
 
     }
